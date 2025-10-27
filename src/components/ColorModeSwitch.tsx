d0c8695ae3601cea13e5424aab810d1d4c8aa1ea
manuelezameta <manuelezameta@gmail.com>
2025-10-27 17:28:24 -0500

    Added ColorMode switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,16 @@
+import { HStack, Switch } from "@chakra-ui/react";
+import { useColorMode } from "./ui/color-mode";
+
+export const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch.Root checked={colorMode == "dark"} onChange={toggleColorMode}>
+        <Switch.HiddenInput />
+        <Switch.Control />
+        <Switch.Label>Dark Mode</Switch.Label>
+      </Switch.Root>
+    </HStack>
+  );
+};
