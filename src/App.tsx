3476a6b49aea770c824b9f7bcc93373d79d631b3
manuelezameta <manuelezameta@gmail.com>
2025-10-27 16:35:22 -0500

    Added responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import { useState } from "react";
 import "./App.css";
-import { Button, HStack } from "@chakra-ui/react";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
+  const showAside = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
-    <>
-      <HStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when={showAside}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
