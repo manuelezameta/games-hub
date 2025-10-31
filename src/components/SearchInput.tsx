97b3c1ac789ffa6f0c5d08f04fbda07eb78fa3cb
manuelezameta <manuelezameta@gmail.com>
2025-10-31 10:55:16 -0500

    Added search logic

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,16 +1,33 @@
 import { Input, InputGroup } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (ref.current) {
+      onSearch(ref.current.value);
+    }
+  };
+
   return (
-    <InputGroup startElement={<BsSearch />}>
-      <Input
-        borderRadius={20}
-        placeholder="Search games..."
-        variant="subtle"
-        size="sm"
-      />
-    </InputGroup>
+    <form onSubmit={submitHandler}>
+      <InputGroup startElement={<BsSearch />}>
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="subtle"
+          size="sm"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
